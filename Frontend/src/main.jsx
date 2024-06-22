3cee8888e79ade86063e05d2085aeb1ffdc57945
Prince Sahu <princesahu169@gmail.com>
2024-06-22 13:14:40 +0530

    Updated

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import Layout from './Layout'
+import Home from './components/Home/Home'
+// import About from './components/About/About'
+import { RouterProvider } from 'react-router-dom'
+import { createBrowserRouter } from 'react-router-dom'
+// import App from './App.jsx'
+const router =createBrowserRouter(
+  [
+    {
+      path:'/',
+      element:<Layout/>,
+      children:[
+        {
+          path:"",
+          element:<Home/>
+        },
+        // {
+        //   path:"/about",
+        //   element:<About/>
+        // },
+        // {
+        //   path:"/contact",
+        //   element:<Contact/>
+        // }
+        
+      ]
 
+    }
+  ]
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} /> 
+
+{/* it takes only Routes */}
+</React.StrictMode>,
 )
