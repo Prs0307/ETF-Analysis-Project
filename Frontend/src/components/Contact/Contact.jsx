6658593d653024637bcf20aa6241552a7b36cae2
Prince Sahu <princesahu169@gmail.com>
2024-06-22 18:55:30 +0530

    router  added

---
+++ Frontend/src/components/Contact/Contact.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+function Contact() {
+  return ( 
+    <>
+      <h1 className="ml-80 mr-60 mt-40 mb-60 text-4xl font-bold sm:text-2xl">Contact Page works</h1>
+    </>
+  )
+}
+
+export default Contact
