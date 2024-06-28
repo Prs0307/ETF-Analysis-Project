99a074b8708bca33cabdfc493c0b16c13c531f77
Prince Sahu <princesahu169@gmail.com>
2024-06-28 22:46:46 +0530

    'db dlt'

--- Frontend/src/components/ETFAnalysis/EtfDetails.jsx
+++ Frontend/src/components/ETFAnalysis/EtfDetails.jsx
@@ -1,20 +1,24 @@
 import { useParams } from "react-router-dom"
-
+import Table from "./Table"
 function EtfDetails() {
   const {id}=useParams()
   return (
-    <div>
-      <div className="w-full bg-primary h-96 text-white">
-        <h1 className="text-3xl">ETF Details For : {id}</h1>
+    <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+     
+      <div className="w-full bg-white text-primary">
+        <h1 className="text-2xl font-bold pt-4 mt-10 text-justify selection:none ml-2">ETF / {id}</h1>
+          
       </div>
-      <div>
-        <h1>
-          Hello This is Page for Perticuler ETFs
-        </h1>
-        <div>
-           
+      <section className="mt-6" id="table">
+        <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+        <Table/>
         </div>
-      </div>
+        <div className="text-center text-primary text-4xl p-10 font-semibold justify-center">
+          ...
+        </div>
+      </section>
+      <section id="filterData" className="mt-6"> 
+      </section>
     </div>
   )
 
