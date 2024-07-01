3a72545f4281c880f6ba48bd2dfcf3dbe3b20101
Anubhav Kushwaha <anubhavkush8@gmail.com>
2024-07-01 13:45:00 +0530

    Update admin.py

--- Backend/ETF_Analysis/ETF_services/admin.py
+++ Backend/ETF_Analysis/ETF_services/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(ETF_holdings)
+admin.site.register(Fundhouse)
+admin.site.register(Stock)
+admin.site.register(ETF)
+admin.site.register(AvailabilityDate)
